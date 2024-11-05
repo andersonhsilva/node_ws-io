19f2e68bfa27504346ccaa349b77658ccafc74b6
andersosn henrique <andderson.h.silva@gmail.com>
2024-11-05 15:26:30 -0300

    preparando a peimeira conexao com socket io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io'; // Importação correta do socket.io
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // Usando createServer diretamente
+
+// Inicializa o socket.io passando o servidor HTTP
+const io = new Server(server);
 
 app.get('/', (request, response) => {
     response.send('Hello World');
@@ -12,7 +16,19 @@ app.get('/json', (request, response) => {
     response.json({ 'message' : 'Hello World' });
 });
 
+// Configuração de eventos para o socket.io (opcional)
+io.on('connect', (socket) => {
+    console.log('Um novo usuário se conectou');
+
+    socket.on('test', (response) => {
+        console.log(response);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
+
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000'); 
 });
-
